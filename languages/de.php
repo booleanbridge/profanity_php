56df0341f11aae5f7703d8358d2c97bc01e746d4
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2023-05-06 19:22:26 +0530

    Optimised the Code

--- languages/de.php
+++ languages/de.php
@@ -1,7 +1,7 @@
 <?php
-if (!isset($bad_words)) {
-    $bad_words = array();
+if (!isset($badWords)) {
+    $badWords = array();
 }
 $temp = array('analritter' => 0, 'arsch' => 1, 'arschficker' => 2, 'arschlecker' => 3, 'arschloch' => 4, 'bimbo' => 5, 'bratze' => 6, 'bumsen' => 7, 'bonze' => 8, 'dödel' => 9, 'fick' => 10, 'ficken' => 11, 'flittchen' => 12, 'fotze' => 13, 'fratze' => 14, 'hackfresse' => 15, 'hure' => 16, 'hurensohn' => 17, 'ische' => 18, 'kackbratze' => 19, 'kacke' => 20, 'kacken' => 21, 'kackwurst' => 22, 'kampflesbe' => 23, 'kanake' => 24, 'kimme' => 25, 'lümmel' => 26, 'MILF' => 27, 'möpse' => 28, 'morgenlatte' => 29, 'möse' => 30, 'mufti' => 31, 'muschi' => 32, 'nackt' => 33, 'neger' => 34, 'nigger' => 35, 'nippel' => 36, 'nutte' => 37, 'onanieren' => 38, 'orgasmus' => 39, 'penis' => 40, 'pimmel' => 41, 'pimpern' => 42, 'pinkeln' => 43, 'pissen' => 44, 'pisser' => 45, 'popel' => 46, 'poppen' => 47, 'porno' => 48, 'reudig' => 49, 'rosette' => 50, 'schabracke' => 51, 'schlampe' => 52, 'scheiße' => 53, 'scheisser' => 54, 'schiesser' => 55, 'schnackeln' => 56, 'schwanzlutscher' => 57, 'schwuchtel' => 58, 'tittchen' => 59, 'titten' => 60, 'vögeln' => 61, 'vollpfosten' => 62, 'wichse' => 63, 'wichsen' => 64, 'wichser' => 65,);
 // add temp to bad words
-$bad_words = array_merge($bad_words, $temp);
+$badWords = array_merge($badWords, $temp);
--- languages/pl.php
+++ languages/pl.php
@@ -1,8 +1,8 @@
 <?php
-if (!isset($bad_words)) {
-    $bad_words = array();
+if (!isset($badWords)) {
+    $badWords = array();
 }
 
 $temp = array('burdel' => 0, 'burdelmama' => 1, 'chuj' => 2, 'chujnia' => 3, 'ciota' => 4, 'cipa' => 5, 'cyc' => 6, 'debil' => 7, 'dmuchać' => 8, 'do kurwy nędzy' => 9, 'dupa' => 10, 'dupek' => 11, 'duperele' => 12, 'dziwka' => 13, 'fiut' => 14, 'gówno' => 15, 'gówno prawda' => 16, 'huj' => 17, 'huj ci w dupę' => 18, 'jajco' => 19, 'jajko' => 20, 'ja pierdolę' => 21, 'jebać' => 22, 'jebany' => 23, 'kurwa' => 24, 'kurwy' => 25, 'kutafon' => 26, 'kutas' => 27, 'lizać pałę' => 28, 'obciągać chuja' => 29, 'obciągać fiuta' => 30, 'obciągać loda' => 31, 'pieprzyć' => 32, 'pierdolec' => 33, 'pierdolić' => 34, 'pierdolnąć' => 35, 'pierdolnięty' => 36, 'pierdoła' => 37, 'pierdzieć' => 38, 'pizda' => 39, 'pojeb' => 40, 'pojebany' => 41, 'popierdolony' => 42, 'robic loda' => 43, 'robić loda' => 44, 'ruchać' => 45, 'rzygać' => 46, 'skurwysyn' => 47, 'sraczka' => 48, 'srać' => 49, 'suka' => 50, 'syf' => 51, 'wkurwiać' => 52, 'zajebisty' => 53,);
 // add temp to bad words
-$bad_words = array_merge($bad_words, $temp);
+$badWords = array_merge($badWords, $temp);
