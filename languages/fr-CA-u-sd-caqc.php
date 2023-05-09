1c16c7dbe8b6d92f2c8d7e34c20a574946c103ab
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2023-05-09 20:42:00 +0530

    chore: merged fr and fr-CA-*

--- languages/fr-CA-u-sd-caqc.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!isset($badWords)) {
-    $badWords = array();
-}
-$temp = array('noune' => 0, 'osti' => 1, 'criss' => 2, 'crisse' => 3, 'calice' => 4, 'tabarnak' => 5, 'viarge' => 6,);
-// add temp to bad words
-$badWords = array_merge($badWords, $temp);
