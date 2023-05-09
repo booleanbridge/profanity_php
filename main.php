c71ef545532280fb3f9c53bbc1c7b750914a01fd
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2023-05-09 18:26:13 +0530

    feat: created countryCodeToLocal function

--- main.php
+++ main.php
@@ -1,5 +1,9 @@
 <?php
 
+// GLOBALS
+// HOLD two letter language codes like en, fr, hi etc
+$languages = array();
+$supportedLanguages = array();
 
 /**
  * Returns the supported languages based on the input HTTP_ACCEPT_LANGUAGE header or similarly formatted string of language codes.
@@ -34,244 +38,623 @@ function getSupportedLanguages(string $httpAcceptLanguage, array $detectedLangua
 }
 
 /**
- * Returns a locale from a country code that is provided.
- * @param $countryCode  ISO 3166-2-alpha 2 country code
- * @return array of locales, formatted like en-US, or empty Array if not found
+ * Use the country code to determine the language
+ * @param $countryCode ISO 3166-2-alpha 2 country code
+ * @return void
  */
-function countryCodeToLocale(string $countryCode): array
+function countryCodeToLanguage(string $countryCode = ""): void
 {
+    global $languages;
+
+    // find the country code from cloudflare header only if $countryCode is empty
+    if (empty($countryCode)) {
+        if (isset($_SERVER["HTTP_CF_IPCOUNTRY"])) {
+            $countryCode = $_SERVER["HTTP_CF_IPCOUNTRY"];
+        } else {
+            $countryCode = "IN";
+        }
+    }
+
     // Locale list taken from:
     // http://stackoverflow.com/questions/3191664/
     // list-of-all-locales-and-their-short-codes
-    $locales = array(
-        'af-ZA',
-        'am-ET',
-        'ar-AE',
-        'ar-BH',
-        'ar-DZ',
-        'ar-EG',
-        'ar-IQ',
-        'ar-JO',
-        'ar-KW',
-        'ar-LB',
-        'ar-LY',
-        'ar-MA',
-        'arn-CL',
-        'ar-OM',
-        'ar-QA',
-        'ar-SA',
-        'ar-SY',
-        'ar-TN',
-        'ar-YE',
-        'as-IN',
-        'az-Cyrl-AZ',
-        'az-Latn-AZ',
-        'ba-RU',
-        'be-BY',
-        'bg-BG',
-        'bn-BD',
-        'bn-IN',
-        'bo-CN',
-        'br-FR',
-        'bs-Cyrl-BA',
-        'bs-Latn-BA',
-        'ca-ES',
-        'co-FR',
-        'cs-CZ',
-        'cy-GB',
-        'da-DK',
-        'de-AT',
-        'de-CH',
-        'de-DE',
-        'de-LI',
-        'de-LU',
-        'dsb-DE',
-        'dv-MV',
-        'el-GR',
-        'en-029',
-        'en-AU',
-        'en-BZ',
-        'en-CA',
-        'en-GB',
-        'en-IE',
-        'en-IN',
-        'en-JM',
-        'en-MY',
-        'en-NZ',
-        'en-PH',
-        'en-SG',
-        'en-TT',
-        'en-US',
-        'en-ZA',
-        'en-ZW',
-        'es-AR',
-        'es-BO',
-        'es-CL',
-        'es-CO',
-        'es-CR',
-        'es-DO',
-        'es-EC',
-        'es-ES',
-        'es-GT',
-        'es-HN',
-        'es-MX',
-        'es-NI',
-        'es-PA',
-        'es-PE',
-        'es-PR',
-        'es-PY',
-        'es-SV',
-        'es-US',
-        'es-UY',
-        'es-VE',
-        'et-EE',
-        'eu-ES',
-        'fa-IR',
-        'fi-FI',
-        'fil-PH',
-        'fo-FO',
-        'fr-BE',
-        'fr-CA',
-        'fr-CH',
-        'fr-FR',
-        'fr-LU',
-        'fr-MC',
-        'fy-NL',
-        'ga-IE',
-        'gd-GB',
-        'gl-ES',
-        'gsw-FR',
-        'gu-IN',
-        'ha-Latn-NG',
-        'he-IL',
-        'hi-IN',
-        'hr-BA',
-        'hr-HR',
-        'hsb-DE',
-        'hu-HU',
-        'hy-AM',
-        'id-ID',
-        'ig-NG',
-        'ii-CN',
-        'is-IS',
-        'it-CH',
-        'it-IT',
-        'iu-Cans-CA',
-        'iu-Latn-CA',
-        'ja-JP',
-        'ka-GE',
-        'kk-KZ',
-        'kl-GL',
-        'km-KH',
-        'kn-IN',
-        'kok-IN',
-        'ko-KR',
-        'ky-KG',
-        'lb-LU',
-        'lo-LA',
-        'lt-LT',
-        'lv-LV',
-        'mi-NZ',
-        'mk-MK',
-        'ml-IN',
-        'mn-MN',
-        'mn-Mong-CN',
-        'moh-CA',
-        'mr-IN',
-        'ms-BN',
-        'ms-MY',
-        'mt-MT',
-        'nb-NO',
-        'ne-NP',
-        'nl-BE',
-        'nl-NL',
-        'nn-NO',
-        'nso-ZA',
-        'oc-FR',
-        'or-IN',
-        'pa-IN',
-        'pl-PL',
-        'prs-AF',
-        'ps-AF',
-        'pt-BR',
-        'pt-PT',
-        'qut-GT',
-        'quz-BO',
-        'quz-EC',
-        'quz-PE',
-        'rm-CH',
-        'ro-RO',
-        'ru-RU',
-        'rw-RW',
-        'sah-RU',
-        'sa-IN',
-        'se-FI',
-        'se-NO',
-        'se-SE',
-        'si-LK',
-        'sk-SK',
-        'sl-SI',
-        'sma-NO',
-        'sma-SE',
-        'smj-NO',
-        'smj-SE',
-        'smn-FI',
-        'sms-FI',
-        'sq-AL',
-        'sr-Cyrl-BA',
-        'sr-Cyrl-CS',
-        'sr-Cyrl-ME',
-        'sr-Cyrl-RS',
-        'sr-Latn-BA',
-        'sr-Latn-CS',
-        'sr-Latn-ME',
-        'sr-Latn-RS',
-        'sv-FI',
-        'sv-SE',
-        'sw-KE',
-        'syr-SY',
-        'ta-IN',
-        'te-IN',
-        'tg-Cyrl-TJ',
-        'th-TH',
-        'tk-TM',
-        'tn-ZA',
-        'tr-TR',
-        'tt-RU',
-        'tzm-Latn-DZ',
-        'ug-CN',
-        'uk-UA',
-        'ur-PK',
-        'uz-Cyrl-UZ',
-        'uz-Latn-UZ',
-        'vi-VN',
-        'wo-SN',
-        'xh-ZA',
-        'yo-NG',
-        'zh-CN',
-        'zh-HK',
-        'zh-MO',
-        'zh-SG',
-        'zh-TW',
-        'zu-ZA',
+    $countryCodeToLocale = array(
+        'ZA' =>
+        array(
+            0 => 'af',
+            1 => 'en',
+            2 => 'ns',
+            3 => 'tn',
+            4 => 'xh',
+            5 => 'zu',
+        ),
+        'ET' =>
+        array(
+            0 => 'am',
+        ),
+        'AE' =>
+        array(
+            0 => 'ar',
+        ),
+        'BH' =>
+        array(
+            0 => 'ar',
+        ),
+        'DZ' =>
+        array(
+            0 => 'ar',
+            1 => 'tz',
+        ),
+        'EG' =>
+        array(
+            0 => 'ar',
+        ),
+        'IQ' =>
+        array(
+            0 => 'ar',
+        ),
+        'JO' =>
+        array(
+            0 => 'ar',
+        ),
+        'KW' =>
+        array(
+            0 => 'ar',
+        ),
+        'LB' =>
+        array(
+            0 => 'ar',
+        ),
+        'LY' =>
+        array(
+            0 => 'ar',
+        ),
+        'MA' =>
+        array(
+            0 => 'ar',
+        ),
+        'CL' =>
+        array(
+            0 => 'ar',
+            1 => 'es',
+        ),
+        'OM' =>
+        array(
+            0 => 'ar',
+        ),
+        'QA' =>
+        array(
+            0 => 'ar',
+        ),
+        'SA' =>
+        array(
+            0 => 'ar',
+        ),
+        'SY' =>
+        array(
+            0 => 'ar',
+            1 => 'sy',
+        ),
+        'TN' =>
+        array(
+            0 => 'ar',
+        ),
+        'YE' =>
+        array(
+            0 => 'ar',
+        ),
+        'IN' =>
+        array(
+            0 => 'as',
+            1 => 'bn',
+            2 => 'en',
+            3 => 'gu',
+            4 => 'hi',
+            5 => 'kn',
+            6 => 'ko',
+            7 => 'ml',
+            8 => 'mr',
+            9 => 'or',
+            10 => 'pa',
+            11 => 'sa',
+            12 => 'ta',
+            13 => 'te',
+        ),
+        'AZ' =>
+        array(
+            0 => 'az',
+            1 => 'az',
+        ),
+        'RU' =>
+        array(
+            0 => 'ba',
+            1 => 'ru',
+            2 => 'sa',
+            3 => 'tt',
+        ),
+        'BY' =>
+        array(
+            0 => 'be',
+        ),
+        'BG' =>
+        array(
+            0 => 'bg',
+        ),
+        'BD' =>
+        array(
+            0 => 'bn',
+        ),
+        'CN' =>
+        array(
+            0 => 'bo',
+            1 => 'ii',
+            2 => 'mn',
+            3 => 'ug',
+            4 => 'zh',
+        ),
+        'FR' =>
+        array(
+            0 => 'br',
+            1 => 'co',
+            2 => 'fr',
+            3 => 'gs',
+            4 => 'oc',
+        ),
+        'BA' =>
+        array(
+            0 => 'bs',
+            1 => 'bs',
+            2 => 'hr',
+            3 => 'sr',
+            4 => 'sr',
+        ),
+        'ES' =>
+        array(
+            0 => 'ca',
+            1 => 'es',
+            2 => 'eu',
+            3 => 'gl',
+        ),
+        'CZ' =>
+        array(
+            0 => 'cs',
+        ),
+        'GB' =>
+        array(
+            0 => 'cy',
+            1 => 'en',
+            2 => 'gd',
+        ),
+        'DK' =>
+        array(
+            0 => 'da',
+        ),
+        'AT' =>
+        array(
+            0 => 'de',
+        ),
+        'CH' =>
+        array(
+            0 => 'de',
+            1 => 'fr',
+            2 => 'it',
+            3 => 'rm',
+        ),
+        'DE' =>
+        array(
+            0 => 'de',
+            1 => 'ds',
+            2 => 'hs',
+        ),
+        'LI' =>
+        array(
+            0 => 'de',
+        ),
+        'LU' =>
+        array(
+            0 => 'de',
+            1 => 'fr',
+            2 => 'lb',
+        ),
+        'MV' =>
+        array(
+            0 => 'dv',
+        ),
+        'GR' =>
+        array(
+            0 => 'el',
+        ),
+        29 =>
+        array(
+            0 => 'en',
+        ),
+        'AU' =>
+        array(
+            0 => 'en',
+        ),
+        'BZ' =>
+        array(
+            0 => 'en',
+        ),
+        'CA' =>
+        array(
+            0 => 'en',
+            1 => 'fr',
+            2 => 'iu',
+            3 => 'iu',
+            4 => 'mo',
+        ),
+        'IE' =>
+        array(
+            0 => 'en',
+            1 => 'ga',
+        ),
+        'JM' =>
+        array(
+            0 => 'en',
+        ),
+        'MY' =>
+        array(
+            0 => 'en',
+            1 => 'ms',
+        ),
+        'NZ' =>
+        array(
+            0 => 'en',
+            1 => 'mi',
+        ),
+        'PH' =>
+        array(
+            0 => 'en',
+            1 => 'fi',
+        ),
+        'SG' =>
+        array(
+            0 => 'en',
+            1 => 'zh',
+        ),
+        'TT' =>
+        array(
+            0 => 'en',
+        ),
+        'US' =>
+        array(
+            0 => 'en',
+            1 => 'es',
+        ),
+        'ZW' =>
+        array(
+            0 => 'en',
+        ),
+        'AR' =>
+        array(
+            0 => 'es',
+        ),
+        'BO' =>
+        array(
+            0 => 'es',
+            1 => 'qu',
+        ),
+        'CO' =>
+        array(
+            0 => 'es',
+        ),
+        'CR' =>
+        array(
+            0 => 'es',
+        ),
+        'DO' =>
+        array(
+            0 => 'es',
+        ),
+        'EC' =>
+        array(
+            0 => 'es',
+            1 => 'qu',
+        ),
+        'GT' =>
+        array(
+            0 => 'es',
+            1 => 'qu',
+        ),
+        'HN' =>
+        array(
+            0 => 'es',
+        ),
+        'MX' =>
+        array(
+            0 => 'es',
+        ),
+        'NI' =>
+        array(
+            0 => 'es',
+        ),
+        'PA' =>
+        array(
+            0 => 'es',
+        ),
+        'PE' =>
+        array(
+            0 => 'es',
+            1 => 'qu',
+        ),
+        'PR' =>
+        array(
+            0 => 'es',
+        ),
+        'PY' =>
+        array(
+            0 => 'es',
+        ),
+        'SV' =>
+        array(
+            0 => 'es',
+        ),
+        'UY' =>
+        array(
+            0 => 'es',
+        ),
+        'VE' =>
+        array(
+            0 => 'es',
+        ),
+        'EE' =>
+        array(
+            0 => 'et',
+        ),
+        'IR' =>
+        array(
+            0 => 'fa',
+        ),
+        'FI' =>
+        array(
+            0 => 'fi',
+            1 => 'se',
+            2 => 'sm',
+            3 => 'sm',
+            4 => 'sv',
+        ),
+        'FO' =>
+        array(
+            0 => 'fo',
+        ),
+        'BE' =>
+        array(
+            0 => 'fr',
+            1 => 'nl',
+        ),
+        'MC' =>
+        array(
+            0 => 'fr',
+        ),
+        'NL' =>
+        array(
+            0 => 'fy',
+            1 => 'nl',
+        ),
+        'NG' =>
+        array(
+            0 => 'ha',
+            1 => 'ig',
+            2 => 'yo',
+        ),
+        'IL' =>
+        array(
+            0 => 'he',
+        ),
+        'HR' =>
+        array(
+            0 => 'hr',
+        ),
+        'HU' =>
+        array(
+            0 => 'hu',
+        ),
+        'AM' =>
+        array(
+            0 => 'hy',
+        ),
+        'ID' =>
+        array(
+            0 => 'id',
+        ),
+        'IS' =>
+        array(
+            0 => 'is',
+        ),
+        'IT' =>
+        array(
+            0 => 'it',
+        ),
+        'JP' =>
+        array(
+            0 => 'ja',
+        ),
+        'GE' =>
+        array(
+            0 => 'ka',
+        ),
+        'KZ' =>
+        array(
+            0 => 'kk',
+        ),
+        'GL' =>
+        array(
+            0 => 'kl',
+        ),
+        'KH' =>
+        array(
+            0 => 'km',
+        ),
+        'KR' =>
+        array(
+            0 => 'ko',
+        ),
+        'KG' =>
+        array(
+            0 => 'ky',
+        ),
+        'LA' =>
+        array(
+            0 => 'lo',
+        ),
+        'LT' =>
+        array(
+            0 => 'lt',
+        ),
+        'LV' =>
+        array(
+            0 => 'lv',
+        ),
+        'MK' =>
+        array(
+            0 => 'mk',
+        ),
+        'MN' =>
+        array(
+            0 => 'mn',
+        ),
+        'BN' =>
+        array(
+            0 => 'ms',
+        ),
+        'MT' =>
+        array(
+            0 => 'mt',
+        ),
+        'NO' =>
+        array(
+            0 => 'nb',
+            1 => 'nn',
+            2 => 'se',
+            3 => 'sm',
+            4 => 'sm',
+        ),
+        'NP' =>
+        array(
+            0 => 'ne',
+        ),
+        'PL' =>
+        array(
+            0 => 'pl',
+        ),
+        'AF' =>
+        array(
+            0 => 'pr',
+            1 => 'ps',
+        ),
+        'BR' =>
+        array(
+            0 => 'pt',
+        ),
+        'PT' =>
+        array(
+            0 => 'pt',
+        ),
+        'RO' =>
+        array(
+            0 => 'ro',
+        ),
+        'RW' =>
+        array(
+            0 => 'rw',
+        ),
+        'SE' =>
+        array(
+            0 => 'se',
+            1 => 'sm',
+            2 => 'sm',
+            3 => 'sv',
+        ),
+        'LK' =>
+        array(
+            0 => 'si',
+        ),
+        'SK' =>
+        array(
+            0 => 'sk',
+        ),
+        'SI' =>
+        array(
+            0 => 'sl',
+        ),
+        'AL' =>
+        array(
+            0 => 'sq',
+        ),
+        'CS' =>
+        array(
+            0 => 'sr',
+            1 => 'sr',
+        ),
+        'ME' =>
+        array(
+            0 => 'sr',
+            1 => 'sr',
+        ),
+        'RS' =>
+        array(
+            0 => 'sr',
+            1 => 'sr',
+        ),
+        'KE' =>
+        array(
+            0 => 'sw',
+        ),
+        'TJ' =>
+        array(
+            0 => 'tg',
+        ),
+        'TH' =>
+        array(
+            0 => 'th',
+        ),
+        'TM' =>
+        array(
+            0 => 'tk',
+        ),
+        'TR' =>
+        array(
+            0 => 'tr',
+        ),
+        'UA' =>
+        array(
+            0 => 'uk',
+        ),
+        'PK' =>
+        array(
+            0 => 'ur',
+        ),
+        'UZ' =>
+        array(
+            0 => 'uz',
+            1 => 'uz',
+        ),
+        'VN' =>
+        array(
+            0 => 'vi',
+        ),
+        'SN' =>
+        array(
+            0 => 'wo',
+        ),
+        'HK' =>
+        array(
+            0 => 'zh',
+        ),
+        'MO' =>
+        array(
+            0 => 'zh',
+        ),
+        'TW' =>
+        array(
+            0 => 'zh',
+        ),
     );
 
-    $languageCodes = array();
-    foreach ($locales as $locale) {
-        $localeRegion = locale_get_region($locale);
-        $localeLanguage = locale_get_primary_language($locale);
-        $localeArray = array(
-            'language' => $localeLanguage,
-            'region' => $localeRegion
-        );
-
-        if (
-            strtoupper($countryCode) == $localeRegion
-        ) {
-            array_push($languageCodes, join('-', $localeArray));
-        }
+    if (isset($countryCodeToLocale[$countryCode])) {
+        array_push($languages, ...$countryCodeToLocale[$countryCode]);
     }
-    return $languageCodes;
 }
 
 /**
@@ -283,18 +666,6 @@ function getHttpAcceptLanguage(): string
 {
     // Base languages
     $httpAcceptLanguage = isset($_SERVER["HTTP_ACCEPT_LANGUAGE"]) ? $_SERVER["HTTP_ACCEPT_LANGUAGE"] : '';
-
-    // find the country code from cloudflare header
-    if (!isset($_SERVER["HTTP_CF_IPCOUNTRY"])) {
-        // for testing. won't run when deployed.
-        $_SERVER["HTTP_CF_IPCOUNTRY"] = "IN";
-    }
-    if (isset($_SERVER["HTTP_CF_IPCOUNTRY"])) {
-        $countryCode = $_SERVER["HTTP_CF_IPCOUNTRY"];
-        $languages = countryCodeToLocale($countryCode);
-        addAdditionalLanguages($httpAcceptLanguage, $languages);
-    }
-
     return $httpAcceptLanguage;
 }
 
